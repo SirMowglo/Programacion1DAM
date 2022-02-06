607ea500e9803dc74a13b1c487c2a708969ec137
Alejandro Damas <aflowv@gmail.com>
2022-02-06 19:50:12 +0100

    ArrayList multidimensional

--- src/Herencias/Ajedrez/Tablero.java
+++ src/Herencias/Ajedrez/Tablero.java
@@ -3,17 +3,13 @@ package Herencias.Ajedrez;
 import java.util.ArrayList;
 
 public class Tablero {
-    private ArrayList<Casilla> tablero;
-
-    public ArrayList<Casilla> getTablero() {
-        return tablero;
-    }
-
+    private ArrayList<ArrayList<Casilla>> tablero;
     public Tablero() {
-        tablero= new ArrayList<Casilla>();
-        for(int numero=1; numero<=8;numero++){
-            for(int letra=1; letra<=8; letra++ ){
-                this.tablero.add(new Casilla(letra, numero));
+        tablero= new ArrayList<ArrayList<Casilla>>();
+        for(int letra=1; letra<=8;letra++){
+            this.tablero.add(letra-1,new ArrayList<Casilla>());
+            for(int numero=1; numero<=8; numero++ ){
+                this.tablero.get(letra-1).add(numero-1,new Casilla(numero,letra));
             }
         }
     }
@@ -21,11 +17,13 @@ public class Tablero {
     @Override
     public String toString() {
         String res="";
-        for(int i = 0; i<=63;i++){
-            if(i==7 || i==15|| i==23||i==31||i==39||i==47||i==55){
-                res += tablero.get(i).toString() +'\n';
-            }else{
-                res += tablero.get(i).toString();
+        for(int letra = 0; letra<=7;letra++){
+            for(int numero=0; numero<=7;numero++){
+                if(numero==7){
+                    res += this.tablero.get(letra).get(numero) +"\n";
+                }else{
+                    res+= this.tablero.get(letra).get(numero);
+                }
             }
         }
         return res;
